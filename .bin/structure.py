b9bbb8f9871d4a93c0b21bb53ef19f83dd0db8eb
va0r <i@vialor.ru>
2024-02-13 22:29:37 +0300

    code review

--- .bin/structure.py
+++ .bin/structure.py
@@ -1,27 +1,27 @@
 import os
 
+if __name__ == '__main__':
+    def print_directory_structure(directory, gitignore_rules, indent=0, file=None):
+        if file is None:
+            file = open("directory_structure.txt", "w", encoding="utf-8")
+        for item in os.listdir(directory):
+            if os.path.isdir(os.path.join(directory, item)):
+                if not any(rule in os.path.join(directory, item) for rule in gitignore_rules):
+                    file.write("    " * indent + f"📁 {item}\n")
+                    print_directory_structure(os.path.join(directory, item), gitignore_rules, indent + 1, file)
+            else:
+                if not any(rule in os.path.join(directory, item) for rule in gitignore_rules):
+                    file.write("    " * indent + f"📄 {item}\n")
 
-def print_directory_structure(directory, gitignore_rules, indent=0, file=None):
-    if file is None:
-        file = open("directory_structure.txt", "w", encoding="utf-8")
-    for item in os.listdir(directory):
-        if os.path.isdir(os.path.join(directory, item)):
-            if not any(rule in os.path.join(directory, item) for rule in gitignore_rules):
-                file.write("    " * indent + f"📁 {item}\n")
-                print_directory_structure(os.path.join(directory, item), gitignore_rules, indent + 1, file)
-        else:
-            if not any(rule in os.path.join(directory, item) for rule in gitignore_rules):
-                file.write("    " * indent + f"📄 {item}\n")
-
-    if indent == 0:
-        file.close()
+        if indent == 0:
+            file.close()
 
 
-directory_path = "../"
-gitignore_path = "../.gitignore"
+    directory_path = "../"
+    gitignore_path = "../.gitignore"
 
-with open(gitignore_path, 'r') as f:
-    gitignore_rules = f.read().splitlines()
+    with open(gitignore_path, 'r') as f:
+        gitignore_rules = f.read().splitlines()
 
-print_directory_structure(directory_path, gitignore_rules)
-print("Структура приложения успешно записана в файл 'directory_structure.txt'")
+    print_directory_structure(directory_path, gitignore_rules)
+    print("Структура приложения успешно записана в файл 'directory_structure.txt'")
