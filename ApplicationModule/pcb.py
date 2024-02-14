de96feef2b12bc185f4b9b05ed26b87012ba1305
va0r <i@vialor.ru>
2024-02-14 14:11:19 +0300

    code review

--- ApplicationModule/pcb.py
+++ ApplicationModule/pcb.py
@@ -4,7 +4,6 @@ init()
 
 
 def print_colored_and_boxed(message):
-    # Создание рамки вокруг сообщения
     border = "═" * (len(message) + 4)
     print(Fore.LIGHTYELLOW_EX + Style.BRIGHT + f"╔{border}╗")
     print("║ " + message + "   ║")
