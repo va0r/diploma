e3fa79b5627f117933605bb1c2ffc4b9aaed5fd5
va0r <i@vialor.ru>
2024-02-14 22:45:54 +0300

    code review

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ git clone https://github.com/va0r/diploma.git ; \
 cd diploma ; \
 python3 -m venv env ; \
 . ./env/bin/activate ; \
-docker compose -f docker-compose.yml up --build ; \
+sudo docker compose -f docker-compose.yml up --build ; \
