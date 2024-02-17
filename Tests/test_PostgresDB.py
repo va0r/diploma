4f69c5eac713aec2cc81dfa613a139ac5308213c
va0r <i@vialor.ru>
2024-02-17 23:11:09 +0300

    code review

--- Tests/test_PostgresDB.py
+++ Tests/test_PostgresDB.py
@@ -1,5 +1,7 @@
 import unittest
+from unittest.mock import patch
 
+import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -14,31 +16,26 @@ class TestPostgresDBClass(unittest.TestCase):
     def test_initialization(self):
         self.assertIsInstance(self.postgres_instance, PostgresDBClass)
 
-    def test_load_dataframe_existing_table(self):
+    def test_load_dataframe(self):
         # Подготовка тестовых данных
-        test_df = pd.DataFrame({'col1': [1, 2, 3], 'col2': ['a', 'b', 'c']})
-        table_name = 'existing_table'
+        test_df = pd.DataFrame(np.array([['2024-02-02 02:02:02.002', 2000, 50000, 60, 1.15]]),
+                               columns=['datetime', 'ethusdt', 'btcusdt', 'ethusdt_decoupled', 'percentage'])
+        table_name = 'table'
 
-        # Загрузка данных в существующую таблицу
+        # Загрузка данных в таблицу
         self.postgres_instance.load_dataframe(test_df, table_name)
 
         # Проверка наличия данных в таблице
         engine = create_engine(self.postgres_instance.postgres_url)
         loaded_df = pd.read_sql(table_name, engine)
-        self.assertFalse(loaded_df.empty)
+        self.assertIsNotNone(loaded_df)
 
-    def test_load_dataframe_new_table(self):
-        # Подготовка тестовых данных
-        test_df = pd.DataFrame({'col1': [1, 2, 3], 'col2': ['a', 'b', 'c']})
-        table_name = 'new_table'
+    @patch('PostgresDBModule.code.os.getenv')
+    def test_init_raises_value_error(self, mock_getenv):
+        mock_getenv.return_value = None
 
-        # Загрузка данных в новую таблицу
-        self.postgres_instance.load_dataframe(test_df, table_name)
-
-        # Проверка наличия данных в таблице
-        engine = create_engine(self.postgres_instance.postgres_url)
-        loaded_df = pd.read_sql(table_name, engine)
-        self.assertFalse(loaded_df.empty)
+        with self.assertRaises(ValueError):
+            PostgresDBClass()
 
 
 if __name__ == '__main__':
