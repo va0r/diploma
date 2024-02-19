058fc1432b8cacce717cd1dc02d9c42345c266a6
va0r <i@vialor.ru>
2024-02-19 12:20:25 +0300

    code review

--- Tests/test_PostgresDB.py
+++ Tests/test_PostgresDB.py
@@ -13,29 +13,30 @@ class TestTablesPostgresDBClass(unittest.TestCase):
         self.postgres_instance = PostgresDBClass()
         test_df = pd.DataFrame(np.array([['2024-02-02 02:02:02.002', 2000, 50000, 60, 1.15]]),
                                columns=['datetime', 'ethusdt', 'btcusdt', 'ethusdt_decoupled', 'percentage'])
+        test_df = test_df.astype({'datetime': 'datetime64[ns]',
+                                   'ethusdt': float,
+                                   'btcusdt': float,
+                                   'ethusdt_decoupled': float,
+                                   'percentage': float})
         self.test_dataframe = test_df
 
-    @patch('pandas.read_sql')
+    @patch('pandas.read_sql', return_value=pd.DataFrame({'datetime': [datetime.now() - timedelta(hours=2)]}))
     def test_load_dataframe_session_existing_append(self, mock_read_sql):
         table_name = 'SESSION'
         self.postgres_instance.metadata.tables = {'SESSION': 'example_table'}
-        existing_df = pd.DataFrame({'datetime': [datetime.now() - timedelta(hours=2)]})
-        mock_read_sql.return_value = existing_df
         self.postgres_instance.load_dataframe(self.test_dataframe, table_name)
         mock_read_sql.assert_called_once_with(table_name, self.postgres_instance.engine, columns=['datetime'])
         self.assertEqual(mock_read_sql.call_count, 1)
 
-    @patch('pandas.read_sql')
+    @patch('pandas.read_sql', return_value=pd.DataFrame({'datetime': [datetime.now() - timedelta(hours=2)]}))
     def test_load_dataframe_session_existing_replace(self, mock_read_sql):
         table_name = 'SESSION'
         self.postgres_instance.metadata.tables = {'SESSION': 'example_table'}
-        existing_df = pd.DataFrame({'datetime': [datetime.now() - timedelta(hours=2)]})
-        mock_read_sql.return_value = existing_df
         self.postgres_instance.load_dataframe(self.test_dataframe, table_name, if_exists='replace')
         mock_read_sql.assert_called_once_with(table_name, self.postgres_instance.engine, columns=['datetime'])
         self.assertEqual(mock_read_sql.call_count, 1)
 
-    @patch('pandas.read_sql')
+    @patch('pandas.read_sql', return_value=pd.DataFrame())
     def test_load_dataframe_session_non_existing(self, mock_read_sql):
         table_name = 'SESSION'
         self.postgres_instance.metadata.tables = {}
@@ -56,10 +57,8 @@ class TestLineAccessPostgresDBClass(unittest.TestCase):
     def test_initialization(self):
         self.assertIsInstance(self.postgres_instance, PostgresDBClass)
 
-    @patch('PostgresDBModule.code.os.getenv')
+    @patch('PostgresDBModule.code.os.getenv', return_value=None)
     def test_init_raises_value_error(self, mock_getenv):
-        mock_getenv.return_value = None
-
         with self.assertRaises(ValueError):
             PostgresDBClass()
 
